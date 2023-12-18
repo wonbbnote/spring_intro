46eaddf28fc2562b8884ffa2bfcdd84ddd94d998
heewon Choi <wonbb3313@gmail.com>
2023-12-18 21:56:54 +0900

    Feat: 회원 서비스 개발- Extract method

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -17,13 +17,16 @@ public class MemberService {
 
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-
-        memberRepository.save(member);
-        return member.getId();
     }
 
 
